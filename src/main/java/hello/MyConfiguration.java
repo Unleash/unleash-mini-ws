78759a35e2f1005d5d9c53d1b1695786b5ab7ab8
Ivar <ivarconr@gmail.com>
2017-03-13 19:16:05 +0100

    inital setup

---
+++ src/main/java/hello/MyConfiguration.java
@@ -0,0 +1,9 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+    //Wire up unleash here!
+}
---
+++ src/main/java/hello/UnleashController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UnleashController {
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="userId", required=false, defaultValue="unknown") String userId, Model model, HttpServletRequest request) {
+        String sessionId = request.getSession().getId();
+
+        boolean enabled = false;
+
+        model.addAttribute("userId", userId);
+        model.addAttribute("sessionId", sessionId);
+        model.addAttribute("toggle", enabled);
+        return "unleash";
+    }
+
+}
